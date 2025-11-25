3870dbd1a3994bbc48a8b9cb2c5c8c7407170ca7
simonharnqvist <sharnqvi@roe.ac.uk>
2025-11-25 16:52:18 +0000

    README

---
+++ README.md
@@ -0,0 +1,13 @@
+# mwis-api
+
+Unofficial and unsanctioned REST API for the Mountain Weather Information Service website. This application scrapes the MWIS website at regular intervals (60 mins by default), formats the scraped HTML to JSON, and saves it in a relational database (currently in-memory DuckDB). FastAPI then serves a simple API that allows programmatic querying of the forecasts.
+
+In scope/requirements:
+* Web scraper for WMIS
+* API
+* Monitoring/observability
+* CI/CD
+* Docker image
+
+Not in scope:
+* GUI (MWIS provides this)
---
+++ tests/test_scrape.py
@@ -0,0 +1,48 @@
+from mwis_api.mwis_scraper.scrape import (
+    Region,
+    get_regions,
+    get_forecast_html,
+    get_forecast_date,
+    clean_string,
+)
+from datetime import date, timedelta
+
+
+@pytest.fixture
+def soup():
+    return get_forecast_html("scottish", "southern-uplands")
+
+
+def test_get_regions():
+    assert get_regions("regions.csv") == [
+        Region(country="scottish", region="the-northwest-highlands"),
+        Region(country="scottish", region="west-highlands"),
+        Region(country="scottish", region="cairngorms-np-and-monadhliath"),
+        Region(country="scottish", region="southeastern-highlands"),
+        Region(country="scottish", region="southern-uplands"),
+        Region(country="english-and-welsh", region="lake-district"),
+        Region(
+            country="english-and-welsh", region="yorkshire-dales-and-north-pennines"
+        ),
+        Region(country="english-and-welsh", region="peak-district"),
+        Region(country="english-and-welsh", region="snowdonia-national-park"),
+        Region(country="english-and-welsh", region="brecon-beacons"),
+    ]
+
+
+def test_get_forecast_html(soup):
+    assert soup.find("title").text == "Southern Uplands Forecast"
+
+
+def test_get_forecast_date(soup):
+    # first date should be either today or tomorrow
+    forecast_date = get_forecast_date(soup, forecast_day="Forecast0")
+    today = date.today()
+    tomorrow = date.today() + timedelta(days=1)
+    assert forecast_date == today or forecast_date == tomorrow
+
+
+def test_clean_string():
+    dirty = "\n this is a dirty string      \r\n"
+    clean = "this is a dirty string"
+    assert clean_string(dirty) == clean
